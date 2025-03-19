eb9ddba3cd6acda52c91703c18d732604c1aa28e
Czipa Szilárd <szilard.czipa@gmail.com>
2025-03-19 19:31:02 +0100

    Made footer

--- js/script.js
+++ js/script.js
@@ -2,3 +2,7 @@ document.getElementById("toggle-nav").addEventListener("click", function () {
   this.classList.toggle("toggle");
   document.querySelector(".nav__list").classList.toggle("hide-nav");
 });
+
+const currentDate = new Date();
+document.querySelector(".copyright-year").textContent =
+  currentDate.getFullYear();
